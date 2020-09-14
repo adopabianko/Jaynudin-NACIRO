6c3e8cd853d2bcc94416f03b932284f254c1dffe
Ado Pabianko <adopabianko@gmail.com>
2020-09-14 19:40:01 +0700

    refactor

--- config/database.go
+++ config/database.go
@@ -4,26 +4,26 @@ import (
 	"fmt"
 	"database/sql"
 	_ "github.com/lib/pq"
-	"github.com/BurntSushi/toml"
+	"github.com/spf13/viper"
 	"log"
 )
 
-type DBConfig struct {
-	DB_Host, DB_User, DB_Password, DB_Name string
-	DB_Port                                int64
-}
-
 func DBConnection() *sql.DB {
-
-	var dbconfig DBConfig
-
-	_, err := toml.DecodeFile(".env.toml", &dbconfig)
+	viper.SetConfigName(".app-config")
+	viper.AddConfigPath(".")
+	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	DBhost := viper.Get("db.host")
+	DBport := viper.Get("db.port")
+	DBname := viper.Get("db.name")
+	DBuser := viper.Get("db.user")
+	DBpassword := viper.Get("db.password")
+
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable",
-		dbconfig.DB_Host, dbconfig.DB_Port, dbconfig.DB_User, dbconfig.DB_Password, dbconfig.DB_Name)
+		DBhost, DBport, DBuser, DBpassword, DBname)
 
 	db, err := sql.Open("postgres", psqlInfo)
 
